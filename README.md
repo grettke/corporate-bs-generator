74239d9dd7f9b31cb36c63ca556fe2136184d4db
Grant Rettke <gcr@wisdomandwonder.com>
2018-07-06 14:14:09 -0500

    README.md, corporate-bs-generator.el: Initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+# corporate-bs-generator - Generate random corporate buzz phrases
+
+*Author:* Grant Rettke <gcr@wisdomandwonder.com><br>
+*URL:* [http://raw.githubusercontent.com/grettke/corporate-bs-generator/master/corporate-bs-generator.el](http://raw.githubusercontent.com/grettke/corporate-bs-generator/master/corporate-bs-generator.el)<br>
+
+
+
+---
+Converted from `corporate-bs-generator.el` by [*el2markdown*](https://github.com/Lindydancer/el2markdown).
---
+++ corporate-bs-generator.el
@@ -0,0 +1,34 @@
+;;; corporate-bs-generator.el --- Generate random corporate buzz phrases  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2018  Grant Rettke, Jann Sharpe
+;; Copyright (C) 2018  Jann Sharpe <sstarwoman@hotmail.com>
+
+;; Author: Grant Rettke <gcr@wisdomandwonder.com>
+;; Author: Jann Sharpe <sstarwoman@hotmail.com>
+;; Homepage: https://github.com/grettke/corporate-bs-generator
+;; URL: http://raw.githubusercontent.com/grettke/corporate-bs-generator/master/corporate-bs-generator.el
+;; Keywords: games
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+
+(provide 'corporate-bs-generator)
+;;; corporate-bs-generator.el ends here
